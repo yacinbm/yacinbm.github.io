b5f0fb96a59528452e88b5cca12d4c0ed6df3501
yacinbm <57112334+yacinbm@users.noreply.github.com>
2020-10-13 14:26:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const context = cast.framework.CastReceiverContext.getInstance();
 context.addCustomMessageListener('urn:x-cast:com.example.castdata', function(customEvent) {
 	if(customEvent.data.type == "message"){
 		document.getElementById("message").innerHTML = customEvent.data.text;
+		console.log(customEvent.data.text)
 	}
 });
 context.start();
