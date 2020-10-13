0e784156f6f527a2bb69f385b6bd97cebf863a3f
yacinbm <57112334+yacinbm@users.noreply.github.com>
2020-10-13 14:53:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 const context = cast.framework.CastReceiverContext.getInstance();
 context.addCustomMessageListener('urn:x-cast:com.example.castdata', function(customEvent) {
-	if(customEvent.data.type == "message"){
+	console.log("Got message!")
+	if(customEvent.type == "message"){
 		document.getElementById("message").innerHTML = customEvent.data.text;
 		console.log(customEvent.data.text)
 	}
