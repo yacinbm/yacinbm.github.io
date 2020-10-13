ed3ee34571cc0f07edcd38f9ff50894a086c9726
yacinbm <57112334+yacinbm@users.noreply.github.com>
2020-10-13 14:59:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 const context = cast.framework.CastReceiverContext.getInstance();
-context.addCustomMessageListener('urn:x-cast:com.yacinbm.com', function(customEvent) {
+context.addCustomMessageListener('urn:x-cast:msg.yacinbm.cast', function(customEvent) {
 	console.log("Got message!")
 	if(customEvent.type == "message"){
 		document.getElementById("message").innerHTML = customEvent.data.text;
